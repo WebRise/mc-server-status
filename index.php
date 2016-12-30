77624542566f0abbb731e7500df7cfe13d02c3e0
mathhulk <contact@theartex.net>
2016-12-30 12:33:16 -0800

    First full release (1.0)

---
+++ index.php
@@ -0,0 +1,161 @@
+<?php
+include('server-status.php');
+$data = status();
+$extra = status('true');
+?>
+<html lang="en">
+	<head>
+		<title>Server Status</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/yeti/bootstrap.min.css" rel="stylesheet">
+	</head>
+	<body>
+		<nav class="navbar navbar-default navbar-fixed-top">
+		  <div class="container">
+			<div class="navbar-header">
+			  <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			  </button>
+			  <a class="navbar-brand" href="#">Server Status</a>
+			</div>
+
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			  <ul class="nav navbar-nav">
+				<li class="active"><a href="#">Example</a></li>
+				<li><a href="https://github.com/mathhulk/mc-server-status">GitHub</a></li>
+				<li><a href="https://mcapi.ca">MCAPI</a></li>
+			  </ul>
+			  <ul class="nav navbar-nav navbar-right">
+				<li><a href="https://theartex.net">made by mathhulk</a></li>
+			  </ul>
+			</div>
+		  </div>
+		</nav>
+		
+		<div class="container">
+			<div class="jumbotron">
+				<h1><br>Server Status</h1>
+				<p>A server status module for websites using the mcapi.ca API.</p>
+				<p><a data-toggle="modal" data-target="#help"class="btn btn-primary btn-md">Information</a></p>
+			</div>
+			
+			<div class="jumbotron">
+				<h2>Default Statistics <small><?php echo $data['error']; ?></small></h2>
+				<table class="table table-striped table-hover ">
+					<thead>
+						<tr>
+							<th>Data</th>
+							<th style="text-align: right">Value</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>IP</td>
+							<td style="text-align: right"><?php echo $data['ip']; ?></td>
+						</tr>
+						<tr>
+							<td>Port</td>
+							<td style="text-align: right"><?php echo $data['port']; ?></td>
+						</tr>
+						<tr>
+							<td>Minecraft Version</td>
+							<td style="text-align: right"><?php echo $data['minecraft']; ?></td>
+						</tr>
+						<tr>
+							<td>Server Protocol</td>
+							<td style="text-align: right"><?php echo $data['protocol']; ?></td>
+						</tr>
+						<tr>
+							<td>Online Players</td>
+							<td style="text-align: right"><?php echo $data['online_players']; ?></td>
+						</tr>
+						<tr>
+							<td>Max Players</td>
+							<td style="text-align: right"><?php echo $data['max_players']; ?></td>
+						</tr>
+						<tr>
+							<td>MOTD</td>
+							<td style="text-align: right"><?php echo $data['motd']; ?></td>
+						</tr>
+						<tr>
+							<td>Friendly MOTD</td>
+							<td style="text-align: right"><?php echo $data['friendly_motd']; ?></td>
+						</tr>
+						<tr>
+							<td>Icon</td>
+							<td style="text-align: right"><?php echo $data['icon']; ?></td>
+						</tr>
+					</tbody>
+				</table>
+				<h2>&nbsp;<br>Extra Statistics <small> <?php echo $extra['error']; ?></small></h2>
+				<table class="table table-striped table-hover ">
+					<thead>
+						<tr>
+							<th>Data</th>
+							<th style="text-align: right">Value</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>IP</td>
+							<td style="text-align: right"><?php echo $extra['ip']; ?></td>
+						</tr>
+						<tr>
+							<td>Port</td>
+							<td style="text-align: right"><?php echo $extra['port']; ?></td>
+						</tr>
+						<tr>
+							<td>Minecraft Version</td>
+							<td style="text-align: right"><?php echo $extra['minecraft']; ?></td>
+						</tr>
+						<tr>
+							<td>Server Version</td>
+							<td style="text-align: right"><?php echo $extra['server']; ?></td>
+						</tr>
+						<tr>
+							<td>Online Players</td>
+							<td style="text-align: right"><?php echo $extra['online_players']; ?></td>
+						</tr>
+						<tr>
+							<td>Max Players</td>
+							<td style="text-align: right"><?php echo $extra['max_players']; ?></td>
+						</tr>
+						<tr>
+							<td>MOTD</td>
+							<td style="text-align: right"><?php echo $extra['motd']; ?></td>
+						</tr>
+						<tr>
+							<td>Map</td>
+							<td style="text-align: right"><?php echo $extra['map']; ?></td>
+						</tr>
+						<tr>
+							<td>Icon</td>
+							<td style="text-align: right"><?php echo $extra['icon']; ?></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<div id="help" class="modal fade">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button class="close" aria-hidden="true" type="button" data-dismiss="modal">&times;</button>
+						<h4 class="modal-title">Information</h4>
+					</div>
+					<div class="modal-body">
+						<p>Out of all the APIs I could find, mcapi.ca seemed to be the most up-to-date. However, when looking at the API, some of its features are unusable and cannot be displayed. At first, I was planning to release default statistics and extra statistics, but mcapi.ca's extensive feature doesn't seem to be working. Along with that, no APIs that fully supported the grasping of server icons could be found. I will try to fix these issues as soon as possible.</p>
+					</div>
+					<div class="modal-footer">
+						<button class="btn btn-danger" type="button" data-dismiss="modal">Close</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
--- server-status.php
+++ server-status.php
@@ -1,1 +1,101 @@
+<?php
 
+// Welcome to the MCAPI server status query.
+// I do not take credit for the MCAPI website or any of its functions.
+// made my mathhulk (https://theartex.net)
+
+function status($enabled = 'false') { //Enable this option to acquire extensive statistics. Only works if enable-query is set to true. (accepts true/false)
+	//Create a function to return values.
+	
+	$server = array(
+		'ip'=>'mc.theartex.net', //Enter the IP of the server you wish to query.
+		'port'=>'25565'); //Enter the port of the server you wish to query.
+		
+	if(empty($server['ip']) || empty($server['port'])) { //Verify that default options are set.
+
+			$status = array(
+				'connection'=>'error', //Set the connection to error to enable checking.
+				'error'=>'Failed to get contents from url (missing IP or port)'); //Grab the error of the response.
+				
+	} elseif($enabled == 'true') { // Grab extra statistics.
+		
+		$query = array(
+			'address'=>'https://mcapi.ca/query/'.$server["ip"].':'.$server["port"].'/extensive', //Grab statistics from URL.
+			'icon'=>'https://mcapi.ca/query/'.$server["ip"].':'.$server["port"].'/icon'); //Grab icon from URL.
+			
+		$response = json_decode(file_get_contents($query['address']), true); //Decode the JSON response.
+		
+		if($response['status'] == false) { //Make sure that the API connected to the server.
+			
+			$status = array(
+				'connection'=>'error', //Set the connection to error to enable checking.
+				'error'=>$response['error'], //Grab the error of the response.
+				'ip'=>$response['hostname'],
+				'port'=>$response['port']);
+				
+		} else {
+			
+			$status = array(
+				'connection'=>'online', //Set the connection to error to enable checking.
+				'error'=>'Connection available (no error to define)', //Grab the error of the response.
+				'ip'=>$response['hostname'],
+				'port'=>$response['port'],
+				'minecraft'=>$response['version'],
+				'server'=>$response['software'],
+				'online_players'=>$response['players']['online'],
+				'max_players'=>$response['players']['max'],
+				'players'=>$response['list'], //This value must be looped.
+				'plugins'=>$response['plugins'], //This value must be looped.
+				'motd'=>$response['motd'], 
+				'map'=>$response['map'], //Returns the default world.
+				'icon'=>'<img src="'.$query['icon'].'" alt="Server Icon" />'); //This value is HTML friendly.
+		
+		}
+				
+	} elseif($enabled == 'false') { //Grab regular statistics.
+		
+		$query = array(
+			'address'=>'https://mcapi.ca/query/'.$server["ip"].':'.$server["port"].'/info', //Grab statistics from URL.
+			'icon'=>'https://mcapi.ca/query/'.$server["ip"].':'.$server["port"].'/icon'); //Grab icon from URL.
+			
+		$response = json_decode(file_get_contents($query['address']), true); //Decode the JSON response.
+		
+		if($response['status'] == false) { //Make sure that the API connected to the server.
+			
+			$status = array(
+				'connection'=>'error', //Set the connection to error to enable checking.
+				'error'=>$response['error'], //Grab the error of the response.
+				'ip'=>$response['hostname'],
+				'port'=>$response['port']);
+				
+			//echo $response['error'];
+				
+		} else {
+			
+			$status = array(
+				'connection'=>'online', //Set the connection to error to enable checking.
+				'error'=>'Connection available (no error to define)', //Grab the error of the response.
+				'ip'=>$response['hostname'],
+				'port'=>$response['port'],
+				'minecraft'=>$response['version'],
+				'protocol'=>$response['protocol'],
+				'online_players'=>$response['players']['online'],
+				'max_players'=>$response['players']['max'],
+				'motd'=>$response['motd'],
+				'icon'=>'<img src="'.$query['icon'].'" alt="Server Icon" />', //This value is HTML friendly.
+				'friendly_motd'=>$response['htmlmotd']); //This value is HTML friendly.
+				
+		}
+				
+	} else {
+
+			$status = array(
+				'connection'=>'error', //Set the connection to error to enable checking.
+				'error'=>'Failed to get contents from url (enabled [true|false])', //Grab the error of the response.
+				'ip'=>$server['ip'],
+				'port'=>$server['port']); //Verify that $enabled is set to true or false only.
+				
+	}
+	
+	return $status; //Grab the value outside of the API.
+}
